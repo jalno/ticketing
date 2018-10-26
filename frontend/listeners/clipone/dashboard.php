375207db7467c0623843140077bf86c78c8625e5
Mahdi Abedi <abedi@jeyserver.com>
2018-10-26 10:21:38 +0330

    compatibility to webuilder/userpanel@247bf5e7b6c9c315f4a43448b7bdafb80e8edada

--- frontend/listeners/clipone/dashboard.php
+++ frontend/listeners/clipone/dashboard.php
@@ -1,23 +1,37 @@
 <?php
 namespace themes\clipone\listeners\ticketing;
-use \packages\base;
-use \packages\base\translator;
-use \packages\userpanel;
-use \packages\ticketing\authorization;
-use \themes\clipone\views\dashboard as view;
-use \themes\clipone\views\dashboard\shortcut;
+use packages\base\{db, translator, db\parenthesis};
+use packages\userpanel;
+use packages\ticketing\{authorization, authentication, ticket, ticket_message};
+use themes\clipone\views\dashboard as view;
+use themes\clipone\views\dashboard\shortcut;
 class dashboard{
 	public function initialize(){
 		$this->addShortcuts();
 	}
 	protected function addShortcuts(){
-		if(authorization::is_accessed('list')){
+		if (authorization::is_accessed("list")) {
+			$ticket = new ticket();
+			$types = authorization::childrenTypes();
+			db::join("userpanel_users", "userpanel_users.id=ticketing_tickets.client", "INNER");
+			db::join("ticketing_tickets_msgs", "ticketing_tickets_msgs.ticket=ticketing_tickets.id", "INNER");
+			if ($types) {
+				$ticket->where("userpanel_users.type", $types, 'in');
+			} else {
+				$ticket->where("userpanel_users.id", authentication::getID());
+			}
+			$ticket->where("ticketing_tickets.status", ticket::answered, "!=");
+			$tickets = $ticket->count();
 			$shortcut = new shortcut("tickets");
-			$shortcut->icon = 'clip-user-6';
-			$shortcut->color = shortcut::teal;
-			$shortcut->title = translator::trans('shortcut.tickets.title');
-			$shortcut->text = translator::trans('shortcut.tickets.text');
-			$shortcut->setLink(translator::trans('shortcut.tickets.link'), userpanel\url('ticketing'));
+			$shortcut->icon = "clip-user-6";
+			if ($tickets) {
+				$shortcut->title = $tickets;
+				$shortcut->text = translator::trans("shortcut.tickets.not.answered");
+				$shortcut->setLink(translator::trans("shortcut.tickets.link"), userpanel\url("ticketing"));
+			} else {
+				$shortcut->text = translator::trans("shortcut.tickets.has.question");
+				$shortcut->setLink(translator::trans("shortcut.tickets.send.ticket"), userpanel\url("ticketing/new"));
+			}
 			view::addShortcut($shortcut);
 		}
 	}
