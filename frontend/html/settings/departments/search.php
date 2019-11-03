6c505fbcc8e088716a8968ee31fe08849f5ea784
HosseinHosni <hosseinhosni7@gmail.com>
2019-11-03 14:06:56 +0330

    #55 Fix translation missing

--- frontend/html/settings/departments/search.php
+++ frontend/html/settings/departments/search.php
@@ -8,11 +8,11 @@ $this->the_header();
 	<?php if(!empty($this->getDepartments())){ ?>
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				<i class="fa fa-university"></i> <?php echo translator::trans('departments'); ?>
+				<i class="fa fa-university"></i> <?php echo t('departments'); ?>
 				<div class="panel-tools">
-					<a class="btn btn-xs btn-link tooltips" title="<?php echo translator::trans('search'); ?>" href="#search" data-toggle="modal" data-original-title=""><i class="fa fa-search"></i></a>
+					<a class="btn btn-xs btn-link tooltips" title="<?php echo t('search'); ?>" href="#search" data-toggle="modal" data-original-title=""><i class="fa fa-search"></i></a>
 					<?php if($this->canAdd){ ?>
-					<a class="btn btn-xs btn-link tooltips" title="<?php echo translator::trans('add'); ?>" href="<?php echo userpanel\url('settings/departments/add'); ?>"><i class="fa fa-plus"></i></a>
+					<a class="btn btn-xs btn-link tooltips" title="<?php echo t('add'); ?>" href="<?php echo userpanel\url('settings/departments/add'); ?>"><i class="fa fa-plus"></i></a>
 					<?php } ?>
 					<a class="btn btn-xs btn-link panel-collapse collapses" href="#"></a>
 				</div>
@@ -26,7 +26,7 @@ $this->the_header();
 						<thead>
 							<tr>
 								<th class="center">#</th>
-								<th><?php echo translator::trans('ticket.title'); ?></th>
+								<th><?php echo t('ticket.title'); ?></th>
 								<?php if($hasButtons){ ?><th></th><?php } ?>
 							</tr>
 						</thead>
@@ -60,7 +60,7 @@ $this->the_header();
 <div class="modal fade" id="search" tabindex="-1" data-show="true" role="dialog">
 	<div class="modal-header">
 		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-		<h4 class="modal-title"><?php echo translator::trans('search'); ?></h4>
+		<h4 class="modal-title"><?php echo t('search'); ?></h4>
 	</div>
 	<div class="modal-body">
 		<form id="departmentsearch" class="form-horizontal" action="<?php echo userpanel\url("settings/departments"); ?>" method="GET">
@@ -70,15 +70,15 @@ $this->the_header();
 				array(
 					'name' => 'id',
 					'type' => 'number',
-					'label' => translator::trans("ticket.id")
+					'label' => t("ticket.id")
 				),
 				array(
 					'name' => 'title',
-					'label' => translator::trans("department.title")
+					'label' => t("department.title")
 				),
 				array(
 					'type' => 'select',
-					'label' => translator::trans('search.comparison'),
+					'label' => t('search.comparison'),
 					'name' => 'comparison',
 					'options' => $this->getComparisonsForSelect()
 				)
@@ -90,8 +90,8 @@ $this->the_header();
 		</form>
 	</div>
 	<div class="modal-footer">
-		<button type="submit" form="departmentsearch" class="btn btn-success"><?php echo translator::trans("search"); ?></button>
-		<button type="button" class="btn btn-default" data-dismiss="modal" aria-hidden="true"><?php echo translator::trans('cancel'); ?></button>
+		<button type="submit" form="departmentsearch" class="btn btn-success"><?php echo t("search"); ?></button>
+		<button type="button" class="btn btn-default" data-dismiss="modal" aria-hidden="true"><?php echo t('cancel'); ?></button>
 	</div>
 </div>
 <?php
