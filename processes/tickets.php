b2bb1f0a20b9c90efd0441430391d20579e47f72
HosseinHosni <hosseinhosni7@gmail.com>
2019-11-08 18:01:26 +0330

    #53 Fix discussion problems

--- processes/tickets.php
+++ processes/tickets.php
@@ -3,7 +3,7 @@ namespace packages\ticketing\processes;
 
 use packages\base;
 use packages\base\{log, Options, Process, Response};
-use packages\ticketing\{Events, Logs, Ticket};
+use packages\ticketing\{Events, Logs, Ticket, Ticket_message};
 use packages\userpanel\{Date, Log as UserpanelLog};
 
 class Tickets extends Process {
@@ -46,10 +46,10 @@ class Tickets extends Process {
 		if ($dryRun) {
 			$log->info("dry-run mode: ON");
 		}
-		// $checkExistBefore = (isset($data['check-exist-before']));
-		// if ($checkExistBefore) {
-		// 	$log->info("check this log is exist before: ON");
-		// }
+		$checkExistBefore = (isset($data['check-exist-before']));
+		if ($checkExistBefore) {
+			$log->info("check this log is exist before: ON");
+		}
 		$log->info("get all tickets with messages");
 		$tickets = new Ticket();
 		$tickets->with("message");
@@ -57,26 +57,37 @@ class Tickets extends Process {
 		$log->reply(count($tickets), " Found!");
 		$log->info("get each ticket messages to make reply log for it");
 		foreach ($tickets as $ticket) {
+			$ticketMessages = (new Ticket_message())->where("ticket", $ticket->id)->orderBy("date", "ASC")->get();
 			$log->info("ticket: #" . $ticket->id . " has (" . count($ticket->message) . ") message");
 			$firstMessage = true;
-			foreach ($ticket->message as $message) {
+			foreach ($ticketMessages as $message) {
 				$log->info("ticket: #" . $ticket->id . " message_id: #" . $message->id);
 				if ($firstMessage) {
 					$log->info("this is first message of ticket and is not reply message, skip...");
 					$firstMessage = false;
 					continue;
 				}
-				// if ($checkExistBefore) {
-				// 	$existBefore = new UserpanelLog();
-				// 	$existBefore = $existBefore->where("type", Logs\tickets\Reply::class)->where("user", $message->user)->where("time", $message->date)->has();
-				// 	if ($existBefore) {
-				// 		$log->info("check-exist-before is ON, this log is exist before, skip...");
-				// 		continue;
-				// 	}
-				// }
+				$userID = $message->user->id;
+				if ($checkExistBefore) {
+					$existBefore = (new UserpanelLog())
+						->where("type", Logs\tickets\Reply::class)
+						->where("user", $userID)
+						->where("time", $message->date)
+						->has();
+					if ($existBefore) {
+						$log->info("check-exist-before is ON, this log is exist before, skip...");
+						continue;
+					}
+				}
+				$getLastLogIP = (new UserpanelLog())
+					->where("user", $userID)
+					->where("time", $message->date, "<")
+					->orderBy("time", "DESC")
+					->getValue("ip");
 				$userpanelLog = new UserpanelLog();
 				$userpanelLog->user = $message->user;
 				$userpanelLog->time = $message->date;
+				$userpanelLog->ip = $getLastLogIP;
 				$userpanelLog->title = t("ticketing.logs.reply", array("ticket_id" => $ticket->id));
 				$userpanelLog->type = Logs\tickets\Reply::class;
 				if (!$dryRun) {
