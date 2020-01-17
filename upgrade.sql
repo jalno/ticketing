721c10f43cb107e1109b8505123e50fa484bdc48
Hossein Hosni <hosni.hossein@gmail.com>
2020-01-17 17:29:43 +0330

    Add upgrade.sql

---
+++ upgrade.sql
@@ -0,0 +1,5 @@
+--
+-- Commit: 1cd11af361c743c5f428f2501153cdae61566fdc
+-- Date:   Fri Jan 17 17:24:54 2020 +0330
+--
+ALTER TABLE `ticketing_departments` ADD `status` TINYINT NOT NULL AFTER `users`; 
