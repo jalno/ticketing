c4f2fd7e619fed988ba1b3830162e14b3ac35eb3
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2020-04-01 01:39:59 +0430

    ability to disable autoclose ticket process

--- controllers/Settings.php
+++ controllers/Settings.php
@@ -7,11 +7,17 @@ use packages\userpanel\{events\General\Settings as Event, events\General\Setting
 
 class Settings implements Controller {
 	public function store(array $inputs): array {
+		function translateCloseTime($hours) {
+			if ($hours) {
+				return t("packages.ticketing.autoclose_time.inHours", array("hour" => $hours));
+			}
+			return t("settings.ticketing.autoclose_time.disable");
+		}
 		$logs = array();
 		$option = Options::get("packages.ticketing.close.respitetime");
 		if (isset($inputs["ticketing_autoclose_time"])) {
-			if (!$option or $option != $inputs["ticketing_autoclose_time"]) {
-				$logs[] = new Log('ticketing_autoclose_time', t("packaeges.ticketing.hour", array("hour" => $option / 3600)), t("packaeges.ticketing.hour", array("hour" => $inputs["ticketing_autoclose_time"])), t('settings.ticketing.autoclose_time'));
+			if ($option != $inputs["ticketing_autoclose_time"]) {
+				$logs[] = new Log('ticketing_autoclose_time', translateCloseTime($option / 3600), translateCloseTime($inputs["ticketing_autoclose_time"]), t('settings.ticketing.autoclose_time'));
 				$option = $inputs["ticketing_autoclose_time"] * 3600;
 			}
 		}
--- listeners/Settings.php
+++ listeners/Settings.php
@@ -8,7 +8,7 @@ use packages\ticketing\controllers\Settings as Controller;
 class Settings {
 	
 	public function init(SettingsEvent $settings){
-		$setting = new SettingsEvent\Setting('ticketing');
+		$setting = new SettingsEvent\Setting("ticketing");
 		$setting->setController(Controller::class);
 		$this->addRegisterItems($setting);
 		$settings->addSetting($setting);
@@ -16,26 +16,30 @@ class Settings {
 
 	private function addRegisterItems(SettingsEvent\Setting $setting) {
 		$setting->addInput(array(
-			'name' => 'ticketing_autoclose_time',
-			'type' => 'number',
-			'min' => 1,
+			"name" => "ticketing_autoclose_time",
+			"type" => "number",
+			"min" => 0,
+			"empty" => true,
+			"zero" => true,
+			"default" => 0
 		));
 		$setting->addField(array(
-			'name' => 'ticketing_autoclose_time',
-			'type' => 'number',
-			'label' => t('settings.ticketing.autoclose_time'),
-			'min' => 1,
+			"name" => "ticketing_autoclose_time",
+			"type" => "number",
+			"label" => t("settings.ticketing.autoclose_time"),
+			"min" => 1,
 			"ltr" => true,
+			"placeholder" => t("settings.ticketing.autoclose_time.disable"),
 			"input-group" => array(
 				"right" => array(
 					array(
 						"type" => "addon",
-						"text" => t("packaeges.ticketing.hour"),
+						"text" => t("packages.ticketing.hour"),
 					),
 				),
 			),
 		));
 		$options = Options::get("packages.ticketing.close.respitetime");
-		$setting->setDataForm('ticketing_autoclose_time', $options ? $options / 3600 : "");
+		$setting->setDataForm("ticketing_autoclose_time", $options ? $options / 3600 : "");
 	}
 }
