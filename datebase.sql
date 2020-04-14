69cc4c92eec558204722c397e50879baf8597d25
Hossein Hosni <hosni.hossein@gmail.com>
2020-04-14 15:47:36 +0430

    Add status to ticketing_departments

--- datebase.sql
+++ datebase.sql
@@ -2,6 +2,7 @@ CREATE TABLE `ticketing_departments` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
 	`users` text COLLATE utf8mb4_general_ci,
+	`status` tinyint(4) NOT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
