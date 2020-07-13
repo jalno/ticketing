399267897d28fe389cce55be65fbca467b9d6895
Hossein Hosni <hosni.hossein@gmail.com>
2020-07-13 14:20:20 +0430

    #76 Add default behaviour for send notification on send and reply ticket in general settings

--- controllers/Settings.php
+++ controllers/Settings.php
@@ -7,13 +7,13 @@ use packages\userpanel\{events\General\Settings as Event, events\General\Setting
 
 class Settings implements Controller {
 	public function store(array $inputs): array {
+		$logs = array();
 		function translateCloseTime($hours) {
 			if ($hours) {
 				return t("packages.ticketing.autoclose_time.inHours", array("hour" => $hours));
 			}
 			return t("settings.ticketing.autoclose_time.disable");
 		}
-		$logs = array();
 		if (isset($inputs["ticketing_autoclose_time"])) {
 			$option = Options::get("packages.ticketing.close.respitetime");
 			if (is_numeric($option)) {
@@ -23,6 +23,23 @@ class Settings implements Controller {
 				$logs[] = new Log('ticketing_autoclose_time', translateCloseTime($option), translateCloseTime($inputs["ticketing_autoclose_time"]), t('settings.ticketing.autoclose_time'));
 				$option = $inputs["ticketing_autoclose_time"] * 3600;
 				Options::save("packages.ticketing.close.respitetime", $option, true);
+			}
+		}
+
+		function translateTriggerNotification($value) {
+			return ($value ? t('settings.ticketing.send.with_notification') : t('settings.ticketing.send.without_notification'));
+		}
+		if (isset($inputs['ticketing_send_trigger_notification'])) {
+			$option = Options::get('packages.ticketing.send.trigger_notification');
+			if ($option != $inputs['ticketing_send_trigger_notification']) {
+				$logs[] = new Log(
+					'ticketing_send_trigger_notification',
+					translateTriggerNotification($option),
+					translateTriggerNotification($inputs['ticketing_send_trigger_notification']),
+					t('settings.ticketing.send.trigger_notification')
+				);
+				$option = $inputs['ticketing_send_trigger_notification'];
+				Options::save('packages.ticketing.send.trigger_notification', $option, true);
 			}
 		}
 		return $logs;
--- listeners/Settings.php
+++ listeners/Settings.php
@@ -41,5 +41,28 @@ class Settings {
 		));
 		$options = Options::get("packages.ticketing.close.respitetime");
 		$setting->setDataForm("ticketing_autoclose_time", $options ? $options / 3600 : "");
+
+		$setting->addInput(array(
+			'name' => 'ticketing_send_trigger_notification',
+			'type' => 'bool',
+		));
+		$setting->addField(array(
+			'name' => 'ticketing_send_trigger_notification',
+			'type' => 'radio',
+			'label' => t('settings.ticketing.send.trigger_notification'),
+			'inline' => true,
+			'options' => array(
+				array(
+					'label' => t('settings.ticketing.send.with_notification'),
+					'value' => 1,
+				),
+				array(
+					'label' => t('settings.ticketing.send.without_notification'),
+					'value' => 0,
+				),
+			),
+		));
+		$options = Options::get("packages.ticketing.send.trigger_notification");
+		$setting->setDataForm("ticketing_send_trigger_notification", $options ? 1 : 0);
 	}
 }
